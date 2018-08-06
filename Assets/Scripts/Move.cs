6030eb2905a7dba5fd16643710fb28693da74b19
Unknown <azzhayman@gmail.com>
2018-08-06 15:57:57 +1000

    Added orbiting camera and independent player movement

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,24 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Move : MonoBehaviour
+public class Move : NetworkBehaviour
 {
 
     public float speed = 5f;
     public Rigidbody rigid;
+    public GameObject cam;
 
 
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            cam.SetActive(false);
+        }
+    }
 
 
 
     // Update is called once per frame
     void Update()
     {
-        float inputH = Input.GetAxis("Horizontal");
-        float inputV = Input.GetAxis("Vertical");
-        rigid.AddForce(new Vector3(inputH, 0, inputV) * speed);
+        if (isLocalPlayer)
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            float inputV = Input.GetAxis("Vertical");
+            Vector3 force = new Vector3(inputH, 0, inputV);
 
+            force = transform.TransformDirection(force);
 
+            rigid.AddForce(force * speed);
+
+        }
     }
 }
