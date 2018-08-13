16b3103e7da2778a1837be60b1d955c9b62b5cff
Unknown <azzhayman@gmail.com>
2018-08-13 15:57:03 +1000

    Added multiplayer player

---
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public PlayerController controller;
+
+    
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizonatl");
+        float inputV = Input.GetAxis("Vertical");
+        controller.Move(inputH, inputV);
+
+    }
+}
